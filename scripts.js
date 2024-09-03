4d75f86dc38d14e0503af78008a5c2434fea8a9d
TablePhoOne <126613800+TablePhoOne@users.noreply.github.com>
2024-09-03 03:40:19 -0400

    new ico , responive test

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 function start () {
-   // (PART A) GET SPLASH SCREEN 
-   let splash = document.getElementById("splash");
- 
-   // (PART B) PLAY BGM & REMOVE SPLASH SCREEN AFTER FADE IN
-   splash.addEventListener("transitionend", () => {
-     document.getElementById("bgm").play();
-     splash.remove();
-   });
- 
-   // (PART C) GO!
-   splash.classList.add("hide");
- }
+  // (PART A) GET SPLASH SCREEN 
+  let splash = document.getElementById("splash");
+
+  // (PART B) PLAY BGM & REMOVE SPLASH SCREEN AFTER FADE IN
+  splash.addEventListener("transitionend", () => {
+    document.getElementById("audio").play();
+    splash.remove();
+  });
+
+  // (PART C) GO!
+  splash.classList.add("hide");
+}
+
